e89eeb75822b35a15e5d11435632ca44036f8d36
SUJAN <sujan@SUJANs-MacBook-Air.local>
2026-06-19 11:39:00 +0545

    learn models

--- mvtProject/mvtApp/models.py
+++ mvtProject/mvtApp/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from datetime import datetime
+from django.core.validators import MaxLengthValidator,MaxValueValidator,MinValueValidator,MinLengthValidator
+from phonenumber_field.modelfields import PhoneNumberField
+from multiselectfield import MultiSelectField
 
 # Create your models here.
 
@@ -7,3 +11,37 @@ class Student(models.Model):
     age =models.IntegerField()
     email = models.EmailField()
     message = models.TextField()
+   
+"""field type , option field ,extranal field , relationship
+pip install django-multiselectfield
+pip install django-phonenumber-field phonenumbers
+""" 
+GENDER_FIELD = (
+    ('male','male'),
+    ('female','female'),
+    ('other','other')
+)
+SUBJECT_FIELD =(
+    ('django','django'),
+    ('python','python')
+)
+class Contact(models.Model):
+    iswork = models.BooleanField(default=True,verbose_name="clic here if it works")
+    name = models.CharField(max_length=200,default="sujan",validators=[MinLengthValidator(4)])
+    message = models.TextField(blank=True)
+    # 32 bits sign --> -2^32 --> 2^32   
+    age = models.IntegerField(default=0,validators=[MinValueValidator(1),MaxValueValidator(100)])
+    stock = models.BigIntegerField()
+    stock_1 = models.SmallIntegerField()
+    stock_1 = models.PositiveSmallIntegerField()
+    email = models.EmailField(null=True, unique=True)
+    image = models.ImageField(upload_to="images")
+    file = models.FileField(upload_to="files")
+    dob = models.DateField(default=datetime.now())
+    date_field = models.DateTimeField(default=datetime.now())
+    creat_at = models.DateField(auto_now_add=True,null=True)
+    update_at = models.DateField(auto_now=True,null=True) #update 
+    gender = models.CharField(max_length=20,choices=GENDER_FIELD,null=True)
+    subject = MultiSelectField(choices=SUBJECT_FIELD,null=True)
+    phone = PhoneNumberField(blank=True,region='NP')
+    
